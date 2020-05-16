2740b0786d59bb0706071dbf103b8923ecade155
aysegulsari <sariaysegulsari@gmail.com>
2020-05-16 22:28:04 +0300

    Added custom user profile model,manager and migrations

---
+++ myEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\u16j23\desktop\udemy\rest api\profiles-rest-api\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
